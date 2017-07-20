7e57fcf6329f9c6760436f1ab18a40814707bc43
damasofc <damasofc@hotmail.com>
2017-07-20 12:05:13 -0600

    Tarea1_Programacion3

---
+++ cmake-build-debug/CMakeFiles/Tarea1.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Tarea1.dir/main.cpp.o"
+  "CMakeFiles/Tarea1.dir/Evaluador.cpp.o"
+  "Tarea1.pdb"
+  "Tarea1"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Tarea1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
